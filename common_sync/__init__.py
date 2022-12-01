16be65424184dd1171ce01a3571068e29439757f
william-cass-wright <william.cass.wright@gmail.com>
2022-12-01 02:55:00 -0800

    make commands

--- common_sync/__init__.py
+++ common_sync/__init__.py
@@ -2,11 +2,7 @@
 """sync common files accross github repos"""
 
 import sys
-
-if sys.version_info >= (3, 8):
-    from importlib import metadata as importlib_metadata
-else:
-    import importlib_metadata
+from importlib import metadata as importlib_metadata
 
 
 def get_version() -> str:
