135c8d244105fdde0e5181e593171949736f8f11
william-cass-wright <william.cass.wright@gmail.com>
2022-12-02 00:50:13 -0800

    stuff

--- common_sync/cli.py
+++ common_sync/cli.py
@@ -6,11 +6,6 @@ from click import echo
 from common_sync.utils import hello
 
 
-def echo_dict(input_dict: dict):
-    for key, val in input_dict.items():
-        echo(f"{key[:18]+'..' if len(key)>17 else key}{(20-int(len(key)))*'.'}{val}")
-
-
 @click.group()
 @click.version_option()
 def cli():
@@ -18,6 +13,6 @@ def cli():
 
 
 @cli.command()
-@cli.argument("name")
+@click.argument("name")
 def hi(name):
     echo(hello(name))
